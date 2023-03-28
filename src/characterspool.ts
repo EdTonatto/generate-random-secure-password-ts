0e87d96499c9db38dd7ead206ba92e94aa6d98f6
Eduardo Tonatto <edtonatto@gmail.com>
2023-03-27 21:13:50 -0300

    :construction: Returning characters pool using Options

--- src/characterspool.ts
+++ src/characterspool.ts
@@ -1,13 +1,35 @@
+// eslint-disable-next-line import/no-unresolved, import/extensions
+import { Options } from './options';
+
 const lowercase = 'abcdefghijklmnopqrstuvwxyz';
 const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const numbers = '0123456789';
-const symbols = '\\!@#$%^&*()+_-=}{[]|:;"/?.><,`~\'';
+const symbols = '\\!@#$%&*(){}[]/.,><';
 const similarCharacters = /[ilLI|`oO0]/g;
 const strictRules = [
   { name: 'lowercase', rule: /[a-z]/ },
   { name: 'uppercase', rule: /[A-Z]/ },
   { name: 'numbers', rule: /[0-9]/ },
-  { name: 'symbols', rule: /[\\!@#$%^&*()+_\-=}{[\]|:;"/?.><,`~']/ },
+  { name: 'symbols', rule: /[\\!@#$%&*(){}[\]/.,><]/ },
 ];
 
-function getCharPool() {}
+function getCharPool(options: Options): string {
+  let pool = '';
+
+  if (options.lowercase) pool += lowercase;
+  if (options.uppercase) pool += uppercase;
+  if (options.numbers) pool += numbers;
+  if (options.symbols) pool += symbols;
+
+  if (!pool)
+    throw new Error(
+      'No characters selected. Select at leat one rule to the character pool.',
+    );
+
+  if (options.excludeSimilarCharacters)
+    pool = pool.replace(similarCharacters, '');
+
+  return pool;
+}
+
+export default getCharPool;
---
+++ src/options.ts
@@ -0,0 +1,9 @@
+export interface Options {
+  length?: number;
+  numbers?: boolean;
+  symbols?: boolean;
+  uppercase?: boolean;
+  lowercase?: boolean;
+  excludeSimilarCharacters?: boolean;
+  strict?: boolean;
+}
