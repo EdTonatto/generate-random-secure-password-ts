7a7f67ff897dc368eb3eaeb36e0e280e2f68dcb1
Eduardo Tonatto <edtonatto@gmail.com>
2023-04-01 02:03:08 -0300

    :white_check_mark: Added test for generate method

---
+++ test/generate.spec.ts
@@ -0,0 +1,59 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable prettier/prettier */
+import generate from "../src/generate";
+
+describe('generate', () => {
+  it('should generate a password with the default options', () => {
+    const password = generate({});
+    expect(password.length).toBe(8);
+    expect(password).toMatch(/[a-z]+/);
+  });
+
+  it('should generate a password with the specified length', () => {
+    const password = generate({ length: 10 });
+    expect(password.length).toBe(10);
+  });
+
+  it('should generate a password with numbers', () => {
+    const password = generate({ numbers: true });
+    expect(password).toMatch(/[0-9]+/);
+  });
+
+  it('should generate a password with symbols', () => {
+    const password = generate({ symbols: true });
+    expect(password).toMatch(/[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]+/);
+  });
+
+  it('should generate a password with uppercase letters', () => {
+    const password = generate({ uppercase: true });
+    expect(password).toMatch(/[A-Z]+/);
+  });
+
+  it('should generate a password with lowercase letters', () => {
+    const password = generate({ lowercase: true });
+    expect(password).toMatch(/[a-z]+/);
+  });
+
+  it('should exclude similar characters', () => {
+    const password = generate({ excludeSimilarCharacters: true });
+    expect(password).not.toMatch(/[il1Lo0O]/);
+  });
+
+  it('should generate a password with all options enabled', () => {
+    const password = generate({
+      length: 12,
+      numbers: true,
+      symbols: true,
+      uppercase: true,
+      lowercase: true,
+      excludeSimilarCharacters: true,
+    });
+    expect(password.length).toBe(12);
+    expect(password).toMatch(/[0-9]+/);
+    expect(password).toMatch(/[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]+/);
+    expect(password).toMatch(/[A-Z]+/);
+    expect(password).toMatch(/[a-z]+/);
+    expect(password).not.toMatch(/[il1Lo0O]/);
+  });
+});
